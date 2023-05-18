d16559665e1a8ac80f7ad4f54f0029e6bd2611b9
lucasvelez78 <lucasvelezv@gmail.com>
2023-05-18 08:13:56 -0500

    no mercadopago

--- routes/paymentRoute.js
+++ routes/paymentRoute.js
@@ -40,12 +40,4 @@ router.post("/create", (req, res) => {
     });
 });
 
-router.get("/feedback", function (req, res) {
-  res.json({
-    Payment: req.query.payment_id,
-    Status: req.query.status,
-    MerchantOrder: req.query.merchant_order_id,
-  });
-});
-
 module.exports = router;
